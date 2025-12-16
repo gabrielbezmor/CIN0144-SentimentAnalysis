1bfd2aeacd43c9b11b0ddf38c36d339aee838de1
hugoniceas <niceasoriginal@gmail.com>
2025-12-16 00:39:37 -0300

    implemented creation of initial LimeExplainer

--- src/logistical_regression.py
+++ src/logistical_regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+from lime import lime_tabular
+import shap
 
 
 # Supondo dados já normalizados
@@ -14,3 +16,10 @@ def GridSearchLinearRegression(X_train, y_train,param_grid, cv):
     best_model = grid_search.best_estimator_
     return best_model
 
+def createLimeExplainer(X_train):
+    explainer = lime_tabular.LimeTabularExplainer()
+    X_train.values,
+    mode='classification',
+    feature_names=X_train.columns,
+    class_names=['0','1'],
+    discretize_continuous=False
